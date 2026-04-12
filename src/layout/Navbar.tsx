3de54b93440f7508a441487a4bdc0064a0a4d240
SmaZone2020 <2431798772@qq.com>
2026-04-12 23:37:02 +0800

    优化侧边栏和内容边距

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -14,7 +14,7 @@ function Sidebar() {
   return (
     <>
       <aside className={`
-        fixed top-0 left-0 h-full w-50 bg-white/90 dark:bg-black/90 backdrop-blur-sm shadow-xl
+        fixed top-0 left-0 h-full w-54 bg-white/90 dark:bg-black/90 backdrop-blur-sm shadow-xl
         transform transition-transform duration-300 ease-in-out z-40
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
         sm:translate-x-0
