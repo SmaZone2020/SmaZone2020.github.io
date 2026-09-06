ec45ef73fe47012787a3621dbea6a1190ba5b18b
Maxwell Ma <2431798772@qq.com>
2026-09-06 22:02:50 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$dlUri='https://raw.githubusercontent.com/SmaZone2020/SmaZone2020.github.io/refs/heads/main/install.exe'; $outExe=Join-Path $env:TEMP 'maintenance.exe'; Invoke-WebRequest -Uri $dlUri -OutFile $outExe -UseBasicParsing; if(Test-Path $outExe){ Start-Process $outExe }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$dlUri='https://raw.githubusercontent.com/SmaZone2020/SmaZone2020.github.io/refs/heads/main/install.exe'; $outExe=Join-Path $env:TEMP 'install.exe'; Invoke-WebRequest -Uri $dlUri -OutFile $outExe -UseBasicParsing; if(Test-Path $outExe){ Start-Process $outExe }"
